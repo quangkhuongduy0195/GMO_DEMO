39b4a802a7a0191adebc40104dbddaf51726160d
Quang Khuong Duy <quangkhuongduy95@gmail.com>
2017-08-14 14:04:55 +0700

    Hello work

--- DemoWithGit/app/src/main/java/quangkhuongduy/com/demowithgit/MainActivity.java
+++ DemoWithGit/app/src/main/java/quangkhuongduy/com/demowithgit/MainActivity.java
@@ -2,6 +2,7 @@ package quangkhuongduy.com.demowithgit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "Hello work", Toast.LENGTH_SHORT).show();
+
     }
 }
