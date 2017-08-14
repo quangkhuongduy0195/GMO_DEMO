4543e55c91e2f98abbaff043fa82f6970e42592b
quangkhuongduy0195 <30995643+quangkhuongduy0195@users.noreply.github.com>
2017-08-14 14:05:50 +0700

    Update MainActivity.java
    delete hello work

--- DemoWithGit/app/src/main/java/quangkhuongduy/com/demowithgit/MainActivity.java
+++ DemoWithGit/app/src/main/java/quangkhuongduy/com/demowithgit/MainActivity.java
@@ -11,7 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, "Hello work", Toast.LENGTH_SHORT).show();
 
     }
 }
