8d8825cf3785e95007a472843bacd2132827b3ef
Quang Khuong Duy <quangkhuongduy95@gmail.com>
2017-08-15 10:56:48 +0700

    Toast Main 2 Activity

--- app/src/main/java/quangkhuongduy/com/demowithgit/Main2Activity.java
+++ app/src/main/java/quangkhuongduy/com/demowithgit/Main2Activity.java
@@ -2,6 +2,7 @@ package quangkhuongduy.com.demowithgit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Toast.makeText(this, "Main 2 Activity", Toast.LENGTH_SHORT).show();
+
+
     }
 }
